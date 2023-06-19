7c47a6103bd1dc2f3b629a61b975a3c96f87a01d
Right-cod <kimangadenis015@gmail.com>
2023-06-19 12:55:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Description 'print alphabets except q and e'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+print ('\n');
+return (0);
+}
